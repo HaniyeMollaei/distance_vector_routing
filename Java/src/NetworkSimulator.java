ab7b61e20df02c99ed0091c20b24a6c13e6b8ac3
Haniye Mollaei <HaniyeMolaei1378@gmail.com>
2021-01-20 12:23:41 +0330

    Main

--- Java/src/NetworkSimulator.java
+++ Java/src/NetworkSimulator.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 
 public class NetworkSimulator {
-    // This is the number of entities in the simulator
+    // This is the number of nodes in the simulator
     public static final int NUMNODES = 4;
 
     // These constants are possible events
@@ -11,12 +11,194 @@ public class NetworkSimulator {
     // Parameters of the simulation
     private boolean linkChanges;
     private static int traceLevel;
-    //private static EventList eventList;
+    private static EventList eventList;
     private static Random rand;
 
     // Data used for the simulation
-    private Node[] entity;
+    private Node[] nodes;
     public static int[][] cost;
     private static double time;
 
+    // Initializes the simulator
+    public NetworkSimulator(boolean hasLinkChange, int trace, long seed)
+    {
+        linkChanges = hasLinkChange;
+        traceLevel = trace;
+        eventList = new EventListImpl();
+        rand = new Random(seed);
+        time = 0.0;
+
+        cost = new int[NUMNODES][NUMNODES];
+        cost[0][0] = 0;
+        cost[0][1] = 1;
+        cost[0][2] = 3;
+        cost[0][3] = 7;
+        cost[1][0] = 1;
+        cost[1][1] = 0;
+        cost[1][2] = 1;
+        cost[1][3] = 999;
+        cost[2][0] = 3;
+        cost[2][1] = 1;
+        cost[2][2] = 0;
+        cost[2][3] = 2;
+        cost[3][0] = 7;
+        cost[3][1] = 999;
+        cost[3][2] = 2;
+        cost[3][3] = 0;
+
+        nodes = new Node[NUMNODES];
+        nodes[0] = new Node_0();
+        nodes[1] = new Node_1();
+        nodes[2] = new Node_2();
+        nodes[3] = new Node_3();
+
+        if (linkChanges)
+        {
+            eventList.add(new Event(10000.0, LINKCHANGE, 0));
+        }
+
+    }
+
+    // Starts the simulation. It will end when no more packets are in the medium
+
+    public void runSimulator()
+    {
+        Event next;
+        Packet p;
+
+        while(true)
+        {
+            next = eventList.removeNext();
+
+            if (next == null)
+            {
+                break;
+            }
+
+            if (traceLevel > 1)
+            {
+                System.out.println();
+                System.out.println("main(): event received.  t=" +
+                        next.getTime() +", node=" +
+                        next.getNode());
+                if (next.getType() == FROMLAYER2)
+                {
+                    p = next.getPacket();
+                    System.out.print("  src=" + p.getSource() + ", ");
+                    System.out.print("dest=" + p.getDest() + ", ");
+                    System.out.print("contents=[");
+                    for (int i = 0; i < NUMNODES - 1; i++)
+                    {
+                        System.out.print(p.getMincost(i) + ", ");
+                    }
+                    System.out.println(p.getMincost(NUMNODES - 1) + "]");
+                }
+                else if (next.getType() == LINKCHANGE)
+                {
+                    System.out.println("  Link cost change.");
+                }
+            }
+
+            time = next.getTime();
+
+            if (next.getType() == FROMLAYER2)
+            {
+                p = next.getPacket();
+                if ((next.getNode() < 0) || (next.getNode() >= NUMNODES))
+                {
+                    System.out.println("main(): Panic. Unknown event node.");
+                }
+                else
+                {
+                    nodes[next.getNode()].update(p);
+                }
+            }
+            else if (next.getType() == LINKCHANGE)
+            {
+                if (time < 10001.0)
+                {
+                    cost[0][1] = 20;
+                    cost[1][0] = 20;
+                    nodes[0].linkCostChangeHandler(1, 20);
+                    nodes[1].linkCostChangeHandler(0, 20);
+                }
+                else
+                {
+                    cost[0][1] = 1;
+                    cost[1][0] = 1;
+                    nodes[0].linkCostChangeHandler(1, 1);
+                    nodes[1].linkCostChangeHandler(0, 1);
+                }
+            }
+            else
+            {
+                System.out.println("main(): Panic.  Unknown event type.");
+            }
+        }
+
+        System.out.println("Simulator terminated at t=" + time +
+                ", no packets in medium.");
+    }
+
+
+    public static void toLayer2(Packet p)
+    {
+        Packet currentPacket;
+        double arrivalTime;
+
+        if ((p.getSource() < 0) || (p.getSource() >= NUMNODES))
+        {
+            System.out.println("toLayer2(): WARNING: Illegal source id in " +
+                    "packet; ignoring.");
+            return;
+        }
+        if ((p.getDest() < 0) || (p.getDest() >= NUMNODES))
+        {
+            System.out.println("toLayer2(): WARNING: Illegal destination id " +
+                    "in packet; ignoring.");
+            return;
+        }
+        if (p.getSource() == p.getDest())
+        {
+            System.out.println("toLayer2(): WARNING: Identical source and " +
+                    "destination in packet; ignoring.");
+            return;
+        }
+        if (cost[p.getSource()][p.getDest()] == 999)
+        {
+            System.out.println("toLayer2(): WARNING: Source and destination " +
+                    "not connected; ignoring.");
+            return;
+        }
+
+
+        if (traceLevel > 2)
+        {
+            System.out.println("toLayer2(): source=" + p.getSource() +
+                    " dest=" + p.getDest());
+            System.out.print("             costs:");
+            for (int i = 0; i < NUMNODES; i++)
+            {
+                System.out.print(" " + p.getMincost(i));
+            }
+            System.out.println();
+        }
+
+        arrivalTime = eventList.getLastPacketTime(p.getSource(), p.getDest());
+        if (arrivalTime == 0.0)
+        {
+            arrivalTime = time;
+        }
+        arrivalTime = arrivalTime + 1.0 + (rand.nextDouble() * 9.0);
+
+        if (traceLevel > 2)
+        {
+            System.out.println("toLayer2(): Scheduling arrival of packet.");
+        }
+
+        currentPacket = new Packet(p);
+        eventList.add(new Event(arrivalTime, FROMLAYER2,
+                currentPacket.getDest(), currentPacket));
+    }
 }
+
